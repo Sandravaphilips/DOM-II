34afadf48ba15288a606f923a322d5578cb6cbc7
Sandrava Philips <lacasandra1@gmail.com>
2019-09-03 15:02:05 +0100

    wheel done

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ destination.forEach((dest) => mouseOver(dest));
 
 // KEYDOWN
 document.addEventListener('keydown', ()=> console.log('Well done!'));
+
+// WHEEL
+function zoom(event) {
+    event.preventDefault();
+    
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    imageContent.style.transform = `scale(${scale})`;
+}
+let imageContent = document.querySelector('.img-content');
+let scale = 1;
+imageContent.onwheel = zoom;
