85cda819e2b450e43a464ed14ab3b8ccbebb339c
Sandrava Philips <lacasandra1@gmail.com>
2019-09-03 13:54:03 +0100

    mouseover done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+// MOUSEOVER
+let destination = document.querySelectorAll('.destination');
+
+function mouseOver (dest) {
+    dest.addEventListener('mouseover', () => dest.style.color = 'blue');
+}
+
+destination.forEach((dest) => mouseOver(dest));
+
+// KEYDOWN
+function keyDown (text) {
+    text.style[font-family] = '150%';
+}
+
+let textContent = document.querySelector('.text-content');
+textContent.addEventListener('keydown', keyDown);
